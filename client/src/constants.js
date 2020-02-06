9d062a3623d3a02f90f86c818a372949192bada7
Kaboo <kaboo.bear@gmail.com>
2020-02-06 20:29:05 +0200

    'deploy'

--- client/src/constants.js
+++ client/src/constants.js
@@ -3,7 +3,7 @@ var API;
 if (process.env.NODE_ENV === "development") {
     API = "http://localhost:5000";
   } else if (process.env.NODE_ENV === "production") {
-    API = "https://heroku-test-kaboo3.herokuapp.com";
+    API = "https://heroku-test-kaboo4.herokuapp.com";
 }
 
 export default API;
